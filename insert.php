c41371d1af6c3e9f26e01e6f4d5fb1abd6d90eaa
nagarajan <myselfnagarajan2312@gmail.com>
2021-11-17 10:11:32 +0530

    init

---
+++ insert.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insert Page page</title>
+</head>
+<body>
+    <center>
+        <?php
+        $dbpassword = getenv("SQL_PASSWORD");
+        $conn = mysqli_connect("sql_container", "root", $dbpassword, "school");
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
+        }
+        // Taking all 5 values from the form data(input)
+        $name =  $_REQUEST['name'];
+        $age = $_REQUEST['age']; 
+        // Performing insert query execution
+        $sql = "INSERT INTO students(name,age)  VALUES ('$name', '$age')";
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully.";
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+</html>
---
+++ myapp.php
@@ -0,0 +1,36 @@
+<html>
+ 	<head>
+  		<title>PHP Test</title>
+ 	</head>
+ 	<body>
+		<?php
+			$dbpassword = getenv('SQL_PASSWORD');
+			$conn = new mysqli("sql_container","root",$dbpassword,"school");
+			if($conn -> connect_error){
+				echo "\nError: Unable to connect: " .$conn -> connect_error;
+			}
+			echo "Connected to the database successfully";
+			$result = $conn->query("SELECT * FROM students");
+			echo "<br>Number of rows: $result->num_rows";
+			$result->close();
+			$conn->close();
+		?>
+		<form method = "post" action = "insert.php">
+         	<table>
+            		<tr>
+               			<td>Name:</td> 
+               			<td><input type = "text" name = "name"></td>
+            		</tr>
+            		<tr>
+               			<td>Age</td>
+               			<td><input type = "text" name = "age"></td>
+           		 </tr>
+            		<tr>
+               			<td>
+                  		<input type = "submit" name = "submit" value = "Submit"> 
+               			</td>
+            		</tr>
+         	</table>
+      		</form>
+ 	</body>
+</html>
