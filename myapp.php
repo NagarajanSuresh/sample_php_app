5d63662f065be9603c31ef492116b5fed6424618
NagarajanSuresh <59112057+NagarajanSuresh@users.noreply.github.com>
2021-12-08 11:36:27 +0530

    Update myapp.php

--- myapp.php
+++ myapp.php
@@ -4,7 +4,7 @@
  	</head>
  	<body>
 		<?php
-			$dbpassword = getenv('SQL_PASSWORD');
+			$dbpassword = "root";
 			$conn = new mysqli("sql_container","root",$dbpassword,"school");
 			if($conn -> connect_error){
 				echo "\nError: Unable to connect: " .$conn -> connect_error;
